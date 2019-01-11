eb39f46d0e2384ce20f0a00d5baca44dc4eafb0a
hnsheth07 <hnsheth@andrew.cmu.edu>
2019-01-11 03:21:24 +0100

    Added averaging of distance measurements

--- src/scripts/decawave.py
+++ src/scripts/decawave.py
@@ -4,7 +4,9 @@ import serial
 import time
 from std_msgs.msg import String
 from sensor_msgs.msg import Range
-import pdb;
+import pdb
+from collections import deque
+
 
 def distance_measurement():
     pub = rospy.Publisher('range', Range, queue_size=10)
@@ -18,24 +20,35 @@ def distance_measurement():
         )
 
         ser.write(b'\r\r')
-        res=ser.read(10)
-        time.sleep(0.5)
+        res=ser.read(100000)
+        time.sleep(1)
         ser.write(b'lec\r')
-
         print(res)
 
+        q = deque()
+        total = 0
         while True:
             res=ser.read(100)
             distance = Range()
+
             if len(res)>0:
                 readings = res.split('\r\n')
                 for reading in readings:
                     if 'DIST' in reading:
                         try:
-                            distance.range = float(reading.split(',')[-1])
-                            distance.header.stamp = rospy.get_rostime()
-                            rospy.loginfo(distance)
-                            pub.publish(distance)
+                            range_deca = float(reading.split(',')[-1])
+                            if range_deca:
+                                q.append(range_deca)
+                                if len(q) <= 10:
+                                    total += range_deca
+                                else:
+                                    removed_element = q.popleft()
+                                    print(q)
+                                    total = total - removed_element + range_deca
+                                    distance.range = total/len(q)
+                                    distance.header.stamp = rospy.get_rostime()
+                                    rospy.loginfo(distance)
+                                    pub.publish(distance)
                         except ValueError as e:
                             rospy.loginfo("A string found")
 
