4b1dc936fe0c70175518f0982836ee05ea5382e1
hnsheth07 <hnsheth@andrew.cmu.edu>
2019-04-19 23:31:20 -0400

    Added rtk gps range mearument publisher

---
+++ src/rtkgps.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+from geometry.msg import PoseWithCovariance
+import numpy as np
+from sensor_msgs.msg import Range
+
+position1 = np.array([0,0,0])
+position2 = np.array([0,0,0])
+
+def calculate_real_range():
+    rospy.init_node('rtk_range', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    pub = rospy.Publisher('rtk_range', Range, queue_size=10)
+    rospy.Subscriber("/piksi/enu_pose_fix",PoseWithCovariance, set_pose_1)
+    rospy.Subsciber("/piksi/enu_pose_fix",PoseWithCovariance, set_pose_2)
+    while not rospy.is_shutdown():
+        real_range = np.linalg.norm(position1-position2)
+        range = Range()
+        range.range = real_range
+        pub.publish(range)
+        rate.sleep()
+
+def set_pose_1(msg):
+    global position1
+    position1 = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
+
+def set_pose_2(msg):
+    global position2
+    position2 = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
+
+if __name__ == '__main__':
+    try:
+        calculate_real_range()
+    except rospy.ROSInterruptException:
+        pass
