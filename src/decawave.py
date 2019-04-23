aa282a978494dafce511cd37edae0d3620b47dbc
Sohil-AK2 <savla.sohil@gmail.com>
2019-04-23 19:21:48 +0000

    Changed topic name

--- src/decawave.py
+++ src/decawave.py
@@ -19,7 +19,7 @@ def get_range(median,mean):
         return median
 
 def distance_measurement():
-    pub = rospy.Publisher('estimate_range', Range, queue_size=10)
+    pub = rospy.Publisher('estimated_range', Range, queue_size=10)
     rospy.init_node('decawave', anonymous=True)
     rate = rospy.Rate(10)
     number_of_elements_to_consider_for_filtering = 10
